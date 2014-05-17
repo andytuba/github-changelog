6c50c6dcf27c97c9c0e0f0b58e0653b6972945ed
andytuba <andytuba@andytuba.com>
2014-05-17 10:26:09 -0500

    refactor core.js from index.js
    separate functionality from CLI

---
+++ core.js
@@ -0,0 +1,142 @@
+#!/usr/bin/env node
+
+// core modules
+var fs = require('fs');
+var path = require('path');
+
+// 3rd party modules
+var Client = require('github');
+var async = require('async');
+var handlebars = require('handlebars');
+
+module.exports.run = function(options) {
+  debugger;
+  if (!options.repo) {
+    throw false;
+  }
+  if (!options.username && !options.owner) {
+    throw 'One of "username" or "owner" options must be provided';
+  }
+  if (!(options.since || options.file)) {
+    throw 'One of "since" or "file" options must be provided';
+  }
+  if (options.file && !fs.existsSync(options.file)) {
+    throw 'File not found: %s', options.file;
+  }
+
+  var templatePath = options.template || path.join(__dirname, 'changelog.hbs');
+  var template = fs.readFileSync(templatePath, 'utf8');
+  var changelog = handlebars.compile(template, {noEscape: true});
+
+
+  var since = options.since || fs.statSync(options.file).mtime.toISOString();
+  var header = options.header || 'Changes since ' + since;
+  var owner = options.owner || options.username;
+
+  var github = new Client({version: '3.0.0'});
+
+  if (options.username && options.password) {
+    github.authenticate({
+      type: 'basic',
+      username: options.username,
+      password: options.password
+    });
+  }
+
+  function fetchIssues(callback) {
+    var page = 1;
+    var limit = 100;
+    var issues = [];
+    function fetch() {
+      github.issues.repoIssues({
+        user: owner,
+        repo: options.repo,
+        state: 'closed',
+        sort: 'updated',
+        since: since,
+        per_page: limit,
+        page: page
+      }, function(err, batch) {
+        if (err) {
+          return callback(err);
+        }
+        issues = issues.concat(batch);
+        if (batch.length === limit) {
+          ++page;
+          fetch();
+        } else {
+          callback(null, issues);
+        }
+      });
+    }
+    fetch();
+  }
+
+  function filterIssues(issues, callback) {
+    if (!options.merged) {
+      process.nextTick(function() {
+        callback(null, issues);
+      });
+    } else {
+      async.filter(issues, function(issue, isMerged) {
+        github.pullRequests.getMerged({
+          user: owner,
+          repo: options.repo,
+          number: issue.number
+        }, function(err, result) {
+          isMerged(!err);
+        });
+      }, function(filtered) {
+        callback(null, filtered);
+      });
+    }
+  }
+
+  function formatChangelog(issues, callback) {
+    process.nextTick(function() {
+      callback(null, changelog({
+        header: header,
+        issues: issues,
+        owner: options.owner,
+        repo: options.repo,
+        baseUrl: ['http://github.com', options.owner, options.repo, ''].join('/')
+      }));
+    });
+  }
+
+  function writeChangelog(text, callback) {
+    if (options.file) {
+      var existing;
+      async.waterfall([
+        function(next) {
+          fs.readFile(options.file, next);
+        }, function(data, next) {
+          existing = data;
+          fs.writeFile(options.file, text, next);
+        }, function(next) {
+          fs.appendFile(options.file, existing, next);
+        }
+      ], callback);
+    } else {
+      process.nextTick(function() {
+        console.log(text);
+        callback(null);
+      });
+    }
+  }
+
+  async.waterfall([
+    fetchIssues,
+    filterIssues,
+    formatChangelog,
+    writeChangelog
+  ], function(err) {
+    if (err) {
+      console.error(err.message);
+      process.exit(1);
+    } else {
+      process.exit(0);
+    }
+  });
+
+};
--- index.js
+++ index.js
@@ -1,13 +1,5 @@
 #!/usr/bin/env node
-
-// core modules
-var fs = require('fs');
-var path = require('path');
-
-// 3rd party modules
-var Client = require('github');
-var async = require('async');
-var handlebars = require('handlebars');
+var core = require('./core.js');
 var program = require('commander');
 
 program
@@ -30,134 +22,12 @@ program
         'The default bundled template generates a list of issues in Markdown')
     .parse(process.argv);
 
-if (!program.repo) {
-  program.help();
-  process.exit(1);
-}
-
-if (!program.username && !program.owner) {
-  console.error('\nOne of "username" or "owner" options must be provided');
-  program.help();
-  process.exit(1);
-}
-
-if (!(program.since || program.file)) {
-  console.error('\nOne of "since" or "file" options must be provided');
-  program.help();
-  process.exit(1);
-}
-
-if (program.file && !fs.existsSync(program.file)) {
-  console.error('\nFile not found: %s', program.file);
-  program.help();
-  process.exit(1);
-}
-
-var templatePath = program.template || path.join(__dirname, 'changelog.hbs');
-var template = fs.readFileSync(templatePath, 'utf8');
-var changelog = handlebars.compile(template, {noEscape: true});
-
-
-var since = program.since || fs.statSync(program.file).mtime.toISOString();
-var header = program.header || 'Changes since ' + since;
-var owner = program.owner || program.username;
-
-var github = new Client({version: '3.0.0'});
-
-if (program.username && program.password) {
-  github.authenticate({
-    type: 'basic',
-    username: program.username,
-    password: program.password
-  });
-}
-
-function fetchIssues(callback) {
-  var page = 1;
-  var limit = 100;
-  var issues = [];
-  function fetch() {
-    github.issues.repoIssues({
-      user: owner,
-      repo: program.repo,
-      state: 'closed',
-      sort: 'created',
-      since: since,
-      per_page: limit,
-      page: page
-    }, function(err, batch) {
-      if (err) {
-        return callback(err);
-      }
-      issues = issues.concat(batch);
-      if (batch.length === limit) {
-        ++page;
-        fetch();
-      } else {
-        callback(null, issues);
-      }
-    });
-  }
-  fetch();
-}
-
-function filterIssues(issues, callback) {
-  if (!program.merged) {
-    process.nextTick(function() {
-      callback(null, issues);
-    });
-  } else {
-    async.filter(issues, function(issue, isMerged) {
-      github.pullRequests.getMerged({
-        user: owner,
-        repo: program.repo,
-        number: issue.number
-      }, function(err, result) {
-        isMerged(!err);
-      });
-    }, function(filtered) {
-      callback(null, filtered);
-    });
-  }
-}
-
-function formatChangelog(issues, callback) {
-  process.nextTick(function() {
-    callback(null, changelog({header: header, issues: issues}));
-  });
-}
-
-function writeChangelog(text, callback) {
-  if (program.file) {
-    var existing;
-    async.waterfall([
-      function(next) {
-        fs.readFile(program.file, next);
-      }, function(data, next) {
-        existing = data;
-        fs.writeFile(program.file, text, next);
-      }, function(next) {
-        fs.appendFile(program.file, existing, next);
-      }
-    ], callback);
-  } else {
-    process.nextTick(function() {
-      console.log(text);
-      callback(null);
-    });
-  }
-}
-
-async.waterfall([
-  fetchIssues,
-  filterIssues,
-  formatChangelog,
-  writeChangelog
-], function(err) {
-  if (err) {
-    console.error(err.message);
+try {
+  core.run(program);
+} catch (e) {
+  if (e) {
+    console.error('\n', e);
+    program.help();
     process.exit(1);
-  } else {
-    process.exit(0);
   }
-});
+}
